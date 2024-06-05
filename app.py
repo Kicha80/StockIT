1323d5e3fd5ed769004bc4a5dbd691e9d70299bb
Kicha80 <kicha80@gmail.com>
2024-06-05 23:26:47 +0530

    Update app.py
    updated- port binding

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import logging
 import os  # Import the os module
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
@@ -61,4 +61,5 @@ def get_top_performers():
         return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
